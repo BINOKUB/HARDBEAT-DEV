767f9b4e7dcb2a4808749683aa2f646b09255092
BINOKUB <binokub@gmail.com>
2026-02-01 17:05:47 -0500

    Update audio.js

--- audio.js
+++ audio.js
@@ -258,4 +258,4 @@ window.playSynthSound = function(seqId, freq, duration, slide, disto) {
     playSynthNote(freq, vol, seqId, false); // Pas d'accent en preview
 };
 
-console.log("AUDIO V8: Accents Synths Acti
+console.log("AUDIO V8: Accents Synths Activés !");
